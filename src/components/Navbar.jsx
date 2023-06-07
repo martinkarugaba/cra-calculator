5e0e7d7db8e67c182da8b4ddcf3da8592c8ce82f
Martin Karugaba <martinkarugaba21@gmail.com>
2023-06-07 23:58:02 +0300

    Add Home Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
-  <div className="flex justify-between items-center px-8 py-4">
+  <div className="flex justify-between items-center px-16 py-6">
     <div>
       <h1 className="text-5xl font-semibold">Math Magicians</h1>
     </div>
 
     <ul className="flex justify-center items-center gap-x-4">
-      <li className="border-r-2 border-gray-950 pr-4">
+      <li className="border-r-2 cursor-pointer border-gray-950 pr-4">
         <NavLink to="/">Home</NavLink>
       </li>
-      <li className="border-r-2 border-gray-950 pr-4">
+      <li className="border-r-2 cursor-pointer border-gray-950 pr-4">
         <NavLink to="calculator">Calculator</NavLink>
       </li>
-      <li className="border-r-2 border-gray-950 pr-4">
+      <li className="border-gray-950 cursor-pointer pr-4">
         <NavLink to="quote">Quote</NavLink>
       </li>
     </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,30 @@
 const Home = () => (
-  <div>
-    <h1>Welcome To Our Page!</h1>
-    <p>
+  <div className="px-16 py-[5rem] h-[90vh]">
+    <h1 className="text-4xl font-medium">Welcome To Our Page!</h1>
+    <p className="mb-8 mt-8 text-lg">
       Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-      Autem enim voluptatibus ipsam eum culpa dicta tempora corporis
-      cum sunt, eius exercitationem rerum nihil illo fuga cumque!
-      Sapiente quaerat a deserunt!
+      Commodi et maxime veniam officiis inventore ducimus quis
+      provident distinctio aliquam ipsa. Cumque labore consequuntur
+      esse provident nulla voluptatum itaque tempora iusto quisquam
+      modi quasi porro ea deleniti natus corrupti laboriosam, rerum
+      enim doloremque minus sint excepturi! Molestias, incidunt
+      tenetur. Cupiditate, fuga. Lorem ipsum dolor sit, amet
+      consectetur adipisicing elit. Exercitationem magni impedit
+      dolor, fuga vel aspernatur unde aliquid consequuntur eligendi
+      veniam facilis, iure culpa sapiente soluta doloribus maxime odit
+      nisi? Molestias!
     </p>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-      Autem enim voluptatibus ipsam eum culpa dicta tempora corporis
-      cum sunt, eius exercitationem rerum nihil illo fuga cumque!
-      Sapiente quaerat a deserunt!
+    <p className="mb-8 text-lg">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+      enim voluptatibus ipsam eum culpa dicta tempora corporis cum
+      sunt, eius exercitationem rerum nihil illo fuga cumque! Sapiente
+      quaerat a deserunt! Lorem ipsum, dolor sit amet consectetur
+      adipisicing elit. Commodi et maxime veniam officiis inventore
+      ducimus quis provident distinctio aliquam ipsa. Cumque labore
+      consequuntur esse provident nulla voluptatum itaque tempora
+      iusto quisquam modi quasi porro ea deleniti natus corrupti
+      laboriosam, rerum enim doloremque minus sint excepturi!
+      Molestias, incidunt tenetur. Cupiditate, fuga.
     </p>
   </div>
 );
