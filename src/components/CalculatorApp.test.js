e2adeb5f1ecba8b3fbde2a1e7f73873fb623eabe
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-09 16:42:49 +0330

    Add the snapshot test and interection test for calculatorapp and display components.

---
+++ src/components/CalculatorApp.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import CalculatorApp from './CalculatorApp';
+import Display from './Display';
+import Digits from './Digits';
+import Operators from './Operators';
+
+jest.mock('./Display', () => () => <div data-testid="display" />);
+jest.mock('./Digits', () => () => <div data-testid="digits" />);
+jest.mock('./Operators', () => () => <div data-testid="operators" />);
+
+describe('CalculatorApp component', () => {
+  it('matches the snapshot', () => {
+    const { container } = render(<CalculatorApp />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders the child components correctly', () => {
+    const { getByTestId } = render(<CalculatorApp />);
+    const display = getByTestId('display');
+    const digits = getByTestId('digits');
+    const operators = getByTestId('operators');
+
+    expect(display).toBeInTheDocument();
+    expect(digits).toBeInTheDocument();
+    expect(operators).toBeInTheDocument();
+  });
+});
